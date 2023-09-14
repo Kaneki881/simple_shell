0a50a9d4d0ededc139855d3cec7b3d09e62f1848
YounesFetouaki <104641370+YounesFetouaki@users.noreply.github.com>
2023-09-14 19:37:12 +0000

    exits

---
+++ exits.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * custom_exit - Function to handle custom shell program termination.
+ * @arg: Array of strings representing command line with arguments.
+ * @line: The user input command.
+ * @exitstatus: Exit status for the shell program.
+ */
+void custom_exit(char *arg[], char *line, int exitstatus)
+{
+	if (arg[1])
+	{
+		if (is_valid_exit_number(arg[1]))
+		{
+			exitstatus = atoi(arg[1]);
+		}
+		else
+		{
+			fprintf(stderr, "./shell: exit: invalid exit code: %s\n", arg[1]);
+			exitstatus = 2;
+		}
+	}
+	else if (exitstatus == -1)
+	{
+		exitstatus = EXIT_SUCCESS;
+	}
+
+	free(line);
+	exit(exitstatus);
+}
+/**
+ * valid_exit - checks if the argument is a valid exit code or not
+ * @arg: argument
+ * Return: 1 if it's a valid exit code, 0 otherwise
+ */
+int valid_exit(char *arg)
+{
+	if (arg[0] != '-' && check_if_numeric(arg))
+		return (1);
+	else
+		return (0);
+}
---
+++ main.h
@@ -0,0 +1,6 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+void custom_exit(char *arg[], char *line, int exitstatus);
+int valid_exit(char *arg);
+#endif
